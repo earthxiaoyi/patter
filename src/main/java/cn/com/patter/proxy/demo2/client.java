a31a2570097a3478438a43ad2c86248abec8580a
JM <JM@JM-PC>
2015-06-23 16:17:07 +0800

    增加代理

--- src/main/java/cn/com/patter/proxy/demo2/client.java
+++ src/main/java/cn/com/patter/proxy/demo2/client.java
@@ -3,11 +3,11 @@ package cn.com.patter.proxy.demo2;
 public class client {
 	
 	public static void main(String[] args) throws Exception {
-		Order order = new Order("java编程思想",222,"光头强");
-		System.out.println(order.toString());
+		//Order order = new Order("java编程思想",222,"光头强");
+		//System.out.println(order.toString());
 		Order o = (Order)ProxyFactory.getProxyObj(Order.class.getName());
 		o.setOrderNum(5, "孜然麻辣熊");
-		System.out.println(o.toString());
+		//System.out.println(o.toString());
 	}
 	
 }
